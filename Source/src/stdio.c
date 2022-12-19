97c1f8fd1f15461e3cef1a895d7d99dab1ce208b
Gordon McNab <gordon.mcnab@brtchip.com>
2022-12-19 13:46:02 +0000

    Complete set of OS syscalls for newlib
    Rename to indicate that it covers all syscalls, not just those for printf.

--- Source/src/stdio.c
+++ Source/src/stdio.c
@@ -1,12 +1,13 @@
 /**
-    @file stdio.c
+    @file bootstrap.c
 
     @brief
-    Code to satisfy undeclared externals in libstdio.a. These will allow
-	printf, fprintf etc to work with UART0. The UART muat be setup before
-	and calls are made to the I/O routines.
-
-    
+    Code to satisfy undeclared externals in newlib. These will allow
+    functions that would normally integrate with the operating system to
+    be used. File system functions will return error codes. Memory 
+    allocation will be pointed to an area for the heap. Standard I/O, 
+	printf, fprintf etc, will be directed to work with UART0. The UART 
+    must be setup before use.    
 **/
 /*
  * ============================================================================
@@ -51,12 +52,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <sys/times.h>
 
 #include <ft900.h>
 
 /* CONSTANTS ***********************************************************************/
 
 /* GLOBAL VARIABLES ****************************************************************/
+
+// Start of available memory from linker script.
+extern unsigned char *_edata;
 
 /* LOCAL VARIABLES *****************************************************************/
 
@@ -66,11 +71,34 @@
 
 /* FUNCTIONS ***********************************************************************/
 
+_ssize_t _read_r (struct _reent *ptr, int fd, const void *buf, size_t size)
+{
+    (void)ptr;
+    if (fd == 0) 
+    {
+		// stdin
+        return (_ssize_t)uart_readn(UART0, buf, size);
+    }
+	else if ((fd == 1) || (fd == 2))
+	{
+		// stdout and stderr
+		return 0;
+	}
+    errno = EBADF;
+    return -1;
+}
+
 _ssize_t _write_r (struct _reent *ptr, int fd, const void *buf, size_t size)
 {
     (void)ptr;
-    if ((fd == 0) || (fd == 1) || (fd == 2))
+    if (fd == 0)
+	{
+		// stdin
+		return 0;
+	}
+	else if ((fd == 1) || (fd == 2))
     {
+		// stdout and stderr
         return (_ssize_t)uart_writen(UART0, buf, size);
     }
     errno = EBADF;
@@ -87,22 +115,26 @@ int _close_r (struct _reent *ptr, int fd)
     errno = EBADF;
     return -1;
 }
+
 int _fstat_r (struct _reent *ptr, int fd, struct stat *st)
 {
     (void)ptr;
-    (void)st;
     if ((fd == 0) || (fd == 1) || (fd == 2))
     {
+		// character-orientated device file
+		st->st_mode = S_IFCHR;
         return 0;
     }
     errno = EBADF;
     return -1;
 }
+
 int _getpid_r (struct _reent *ptr)
 {
     (void)ptr;
     return 0;
 }
+
 int _isatty_r (struct _reent *ptr, int fd)
 {
     (void)ptr;
@@ -111,13 +143,16 @@ int _isatty_r (struct _reent *ptr, int fd)
     errno = ENOTTY;
     return 0;
 }
+
 int _kill_r (struct _reent *ptr, int pid, int sig)
 {
     (void)ptr;
     (void)pid;
     (void)sig;
-    return 0;
+    errno = ESRCH;
+    return -1;
 }
+
 _off_t _lseek_r (struct _reent *ptr, int fd, _off_t offset, int whence)
 {
     (void)ptr;
@@ -128,9 +163,118 @@ _off_t _lseek_r (struct _reent *ptr, int fd, _off_t offset, int whence)
     errno = EBADF;
     return (off_t)-1;
 }
+
 void *_sbrk_r (struct _reent *ptr, ptrdiff_t diff)
 {
     (void)ptr;
     (void)diff;
-    return NULL;
+    static unsigned char *p_heap = NULL;
+    void *ret;
+    // first time in here return address immediately above data section.
+    if (p_heap == NULL) 
+    {
+        p_heap = (unsigned char *)&_edata;
+    }
+    ret = (void *)p_heap;
+    // pointer difference is in bytes.
+    // always allow more data, cannot check for clash with stacks
+    p_heap += diff;
+    return ret;
+}
+
+int _execve_r (struct _reent *ptr, const char *path, char *const *argv, char *const *envp)
+{
+    (void)ptr;
+    (void)path;
+    (void)argv;
+    (void)envp;
+    errno = EACCES;
+    return -1;
+}
+
+int _fcntl_r (struct _reent *ptr, int fd, int cmd, int args)
+{
+    (void)ptr;
+    (void)fd;
+    (void)cmd;
+    (void)args;
+    errno = EACCES;
+    return -1;
+}
+
+int _fork_r (struct _reent *ptr)
+{
+    (void)ptr;
+    errno = ENOSYS;
+    return -1;
+}
+
+int _link_r (struct _reent *ptr, const char *old, const char *new)
+{
+    (void)ptr;
+    (void)old;
+    (void)new;
+    errno = EACCES;
+    return -1;
+}
+
+int _mkdir_r (struct _reent *ptr, const char *path, int mode)
+{
+    (void)ptr;
+    (void)path;
+    (void)mode;
+    errno = EACCES;
+    return -1;
+}
+
+int _open_r (struct _reent *ptr, const char *path, int flags, int mode)
+{
+    (void)ptr;
+    (void)path;
+    (void)flags;
+    (void)mode;
+    errno = EACCES;
+    return -1;
+}
+
+int _rename_r (struct _reent *ptr, const char *old, const char *new)
+{
+    (void)ptr;
+    (void)old;
+    (void)new;
+    errno = EACCES;
+    return -1;
+}
+
+int _stat_r (struct _reent *ptr, const char *path, struct stat *st)
+{
+    (void)ptr;
+    (void)path;
+    (void)st;
+    errno = EACCES;
+    return -1;
+}
+
+_CLOCK_T_ _times_r (struct _reent *ptr, struct tms *time)
+{
+    (void)ptr;
+    (void)time;
+    errno = ECHILD;
+    return (clock_t) -1;
+}
+
+int _unlink_r (struct _reent *ptr, const char *path)
+{
+    (void)ptr;
+    (void)path;
+    errno = EACCES;
+    return -1;
+}
+
+int _wait_r (struct _reent *ptr, int *pid)
+{
+    (void)ptr;
+    (void)pid;
+    errno = ECHILD;
+    return -1;
 }
