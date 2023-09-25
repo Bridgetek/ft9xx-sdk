60ec7870ae3f428cec120cf6a395ab52a455a4a5
Ho Tan Dat <dat.hotan@brtchip.com>
2023-09-25 12:32:50 +0700

    Update the CMake for the ADC Example 1
     - Make the user cmake more shorter
    
    Related: #28

--- Examples/toolchain_ft9xx.cmake
+++ Examples/toolchain_ft9xx.cmake
@@ -2,6 +2,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR FT9XX)
 
+# This one use to by pass the test build for compiler
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+
 # Search command for Linux/Windows
 if(MINGW OR CYGWIN OR WIN32)
     # Get the path form the binutils search
@@ -48,3 +51,105 @@ set(TOOLCHAIN_HARDWARE_INCLUDE     "${TOOLCHAIN_DIR}/hardware/include")
 set(TOOLCHAIN_HARDWARE_LIB         "${TOOLCHAIN_DIR}/hardware/lib")
 set(TOOLCHAIN_HARDWARE_LIB_DEBUG   "${TOOLCHAIN_DIR}/hardware/lib/Debug")
 set(TOOLCHAIN_HARDWARE_LIB_RELEASE "${TOOLCHAIN_DIR}/hardware/lib/Release")
+
+macro(ft9xx_setup_project projectName)
+    set(EXECUTABLE ${projectName}.elf)
+
+    # Language setup
+    enable_language(C ASM)
+
+    # Add default include directory for project
+    include_directories(
+        Sources
+        Includes
+        ${TOOLCHAIN_HARDWARE_INCLUDE}
+    )
+
+    # Add default compiler option for project
+    add_compile_options(
+        -fvar-tracking-assignments
+        -Wall
+        -c
+        -fmessage-length=0
+        -ffunction-sections
+    )
+
+    # Add default linker option for project
+    add_link_options(
+        -Wl,--gc-sections
+        -Wl,--entry=_start
+    )
+
+    link_libraries(-lc -lstub)
+endmacro()
+
+macro(ft9xx_target_add_libraries target libraries)
+    target_link_libraries(${target} PRIVATE ${libraries})
+endmacro()
+
+macro(ft9xx_set_project_chipset excutable chipset)
+    if (${chipset} MATCHES FT90X)
+        target_compile_definitions(${excutable} PRIVATE -D__FT900__)
+        target_link_libraries(${excutable} PRIVATE -lft900)
+        set(ENV{FT9XX_OUTPUT_FOLDER_PRE} FT90X)
+    elseif (${chipset} MATCHES FT93X)
+        target_compile_definitions(${excutable} PRIVATE -D__FT930__)
+        target_link_options(${excutable} PRIVATE -mft32b -mcompress)
+        target_link_libraries(${excutable} PRIVATE -lft930)
+        set(ENV{FT9XX_OUTPUT_FOLDER_PRE} FT93X)
+    else ()
+        message(FATAL_ERROR "The TARGET must me FT90X or FT93X.")
+    endif()
+endmacro()
+
+macro(ft9xx_set_project_build_type excutable mode)
+    # Setup library for linker step
+    if (${mode} MATCHES Debug)
+        target_link_libraries(${excutable} PRIVATE -L"${TOOLCHAIN_HARDWARE_LIB_DEBUG}")
+        target_compile_options(${excutable} PRIVATE -g -Og)
+        set(ENV{FT9XX_OUTPUT_FOLDER_POST} Debug)
+    elseif (${mode} MATCHES Release)
+        target_link_libraries(${excutable} PRIVATE -L"${TOOLCHAIN_HARDWARE_LIB_RELEASE}")
+        target_compile_options(${excutable} PRIVATE -O3)
+        set(ENV{FT9XX_OUTPUT_FOLDER_POST} Release)
+    else ()
+        message(FATAL_ERROR "The build type should be Debug or Release.")
+    endif()
+
+    message(STATUS "ENV{FT9XX_OUTPUT_FOLDER_PRE}: $ENV{FT9XX_OUTPUT_FOLDER_PRE}")
+
+    # Set the ouput folder for the elf, bin, map,...
+    set(ENV{FT9XX_OUTPUT_FOLDER_NAME} $ENV{FT9XX_OUTPUT_FOLDER_PRE}_$ENV{FT9XX_OUTPUT_FOLDER_POST})
+    set(ENV{FT9XX_EXECUTABLE_OUTPUT_PATH} ${CMAKE_SOURCE_DIR}/$ENV{FT9XX_OUTPUT_FOLDER_NAME})
+
+    set_target_properties(${excutable}
+        PROPERTIES
+        RUNTIME_OUTPUT_DIRECTORY "$ENV{FT9XX_EXECUTABLE_OUTPUT_PATH}"
+    )
+
+    # Print the size of excutable file after build *.elf successfully
+    add_custom_command(TARGET ${excutable}
+        POST_BUILD
+        # Create bin file
+        COMMAND ${CMAKE_OBJCOPY} --output-target binary
+            $ENV{FT9XX_EXECUTABLE_OUTPUT_PATH}/${PROJECT_NAME}.elf
+            $ENV{FT9XX_EXECUTABLE_OUTPUT_PATH}/${PROJECT_NAME}.bin
+    )
+endmacro()
+
+macro(ft9xx_enable_map_file excutable output_path)
+    target_link_options(${excutable} PRIVATE
+        # Enable the map file
+        -Wl,-Map=${output_path}
+        # Add cross-reference table to the map file
+        -Wl,--cref
+    )
+endmacro()
+
+macro(ft9xx_show_image_size target excutable_path)
+    add_custom_command(TARGET ${target}
+        POST_BUILD
+        # Print out size of image
+        COMMAND ${CMAKE_SIZE_UTIL} --format=berkeley -x ${excutable_path}
+    )
+endmacro()
